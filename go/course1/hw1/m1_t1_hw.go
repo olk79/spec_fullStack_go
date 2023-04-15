2774212dd92b3ea6f89263338c8588d14174903f
OleiUs <oleius@gmail.com>
2023-04-15 22:27:56 +0000

    first task done

--- go/course1/hw1/m1_t1_hw.go
+++ go/course1/hw1/m1_t1_hw.go
@@ -8,3 +8,19 @@
 Выход: стоимость поездки в рублях
 */
 package main
+
+import "fmt"
+
+func main() {
+
+	const fuelPrice = 48.0
+	var dist, consumption float32
+
+	fmt.Println("Введите дистанцию в км от 50 до 10000")
+	fmt.Scanf("%g/n", &dist)
+	fmt.Println("Введите расход в литрах от 5 до 25")
+	fmt.Scanf("%g/n", &consumption)
+
+	fmt.Printf("Стоимость поездки, составила: %.2f руб \n", (dist / 100 * consumption * fuelPrice))
+
+}
